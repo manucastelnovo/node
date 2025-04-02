c7d050e1869099b90a455abd604327b34c0e0916
manucastelnovo <marnutskate@gmail.com>
2025-04-02 15:15:23 -0300

    modify app.js

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,4 +1,13 @@
 const templateExports = require('./js-fundation/01-template')
 require('./js-fundation/02-destructiring')
+const { getUserById } = require('./js-fundation/03-callbacks')
 
 // console.log(templateExports.emailTemplate)
+
+getUserById(1, function(error, user){
+    if(error){
+        throw new Error(error)
+    }
+    console.log(user)
+
+})
