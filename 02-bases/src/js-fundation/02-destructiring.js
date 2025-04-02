c9a3ee76a63e2ab1a272706dd07a0f78eb7b6f0d
manucastelnovo <marnutskate@gmail.com>
2025-04-02 14:23:30 -0300

    destructuring array

---
+++ 02-bases/src/js-fundation/02-destructiring.js
@@ -0,0 +1,10 @@
+// console.log(process.env)
+
+
+const { USERNAME,WT_SESSION } = process.env
+
+// console.table({USERNAME, WT_SESSION})
+
+const characters = ['Flash', 'Superman', 'Green Lantern', 'batman']
+
+const [ , , , batman ]= characters
