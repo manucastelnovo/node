3699d0747d07828acacf1fa899c40f242a742835
manucastelnovo <marnutskate@gmail.com>
2025-04-02 23:10:53 -0300

    checkpoint

--- 02-bases/src/js-fundation/05-factory.js
+++ 02-bases/src/js-fundation/05-factory.js
@@ -1,11 +1,13 @@
+const { v4:uuidv4 } = require('uuid')
+const getAge = require('get-age')
 const obj = { name:'John', birtdata: '1995-09-25'}
 
 const buildPerson = ({name, birthdate})=>{
     return {
-        id: new Date().getTime(),
+        id: uuidv4(),
         name:name,
         birtdate:birthdate,
-        age: new Date().getFullYear() - new Date(birthdate).getFullYear()
+        age: getAge(birtdata)
     }
 }
 
