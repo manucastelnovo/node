04edea1b1edd158740e6e30e8732cadc290152a6
manucastelnovo <marnutskate@gmail.com>
2025-04-03 13:42:23 -0300

    factory builder

--- 02-bases/src/plugins/http-client.plugin.js
+++ 02-bases/src/plugins/http-client.plugin.js
@@ -1,19 +1,19 @@
 const axios = require("axios");
 
-const buildMakeHttpClient = ({ httpApi }) => {
+const buildMakeHttpClient = (httpApi) => {
   return ({ header }) => {
     return {
       get: async (url) => {
-        return await httpApi.get({ url, header });
+        return await httpApi.get(url, { header });
       },
       post: async (url, body) => {
-        return await httpApi.post({ url, data: body, header });
+        return await httpApi.post(url, { data: body, header });
       },
       put: async (url, body) => {
-        return await httpApi.put({ url, body, header });
+        return await httpApi.put(url, { body, header });
       },
       delete: async (url) => {
-        return await httpApi.delete({ url, header });
+        return await httpApi.delete(url, { header });
       },
     };
   };
